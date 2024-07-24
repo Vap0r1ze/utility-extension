ae7a02a6623f08fc7f4f860e888610fdba6c8748
Vap0r1ze <superdash993@gmail.com>
2024-07-24 07:31:24 -0400

    ok real

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from 'wxt';
+import { join } from 'node:path';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
+
+  runner: {
+    chromiumProfile: join(process.cwd(), '.wxt/chromium-profile'),
+    keepProfileChanges: true,
+    profileCreateIfMissing: true,
+  },
+
+  manifest: {
+    permissions: [
+      'activeTab',
+      'storage',
+    ],
+    optional_permissions: [
+
+    ],
+  },
 });
